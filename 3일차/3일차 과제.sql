73c2e109f190305bd903ccefc7dc4b0b298d1903
dddops <Eopseum0@gmail.com>
2025-09-04 18:28:16 +0900

    3일차 제출

---
+++ 3일차/3일차 과제.sql
@@ -0,0 +1,44 @@
+wwwwwwwwwwwwwwwwwww-- CREATE DATABASE db_day_3
+-- CREATE table employees (
+-- 	id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(100),
+--     position VARCHAR(100),
+--     salary DECIMAL(10,2)
+-- );
+
+-- INSERT INTO employees(name, position, salary) VALUES 
+-- ('혜린', 'PM', 90000),
+-- ('은우', 'Frontend', 80000),
+-- ('가을', 'Backend', 92000),
+-- ('지수', 'Frontend', 7800),
+-- ('민혁', 'Frontend', 96000),
+-- ('하온', 'Backend', 130000);
+
+-- SET SQL_SAFE_UPDATES = 0;
+-- Delete FROM employees;
+-- SELECT * from employees
+-- SELECT name, salary FROM employees where position = 'Frontend' and salary <= 90000;
+
+-- UPDATE employees SET salary= salary* 1.1 WHERE position = 'PM';
+-- SELECT name, salary From employees where position = 'PM'
+
+-- update employees SET salary = salary *1.05 where position = 'Backend';
+-- SELECT name, salary FROM employees where position = 'BackEnd'
+
+-- DELETE FROM employees WHERE name = '민혁';
+-- SELECT * from employees
+
+
+-- SELECT position, avg(salary) as average_salary from employees group by position;
+
+-- DROP TABLE employees;
+
+
+	
+    
+
+    
+    
+    
+
+
---
+++ 3일차/employees.md
@@ -0,0 +1,19 @@
+![alt text](image.png)
+
+1. **`Frontend` 직책을 가진 직원 중에서 연봉이 90000 이하인 직원의 이름과 연봉을 조회하세요.**
+   ![alt text](image-1.png)
+
+2. **`PM` 직책을 가진 모든 직원의 연봉을 10% 인상한 후 그 결과를 확인하세요.**
+   ![alt text](image-2.png)
+
+3. **모든 `Backend`' 직책을 가진 직원의 연봉을 5% 인상하세요.**
+   ![alt text](image-3.png)
+
+4. **민혁 사원의 데이터를 삭제하세요**
+   ![alt text](image-4.png)
+
+5. **모든 직원을 `position` 별로 그룹화하여 각 직책의 평균 연봉을 계산하세요.**
+   ![alt text](image-5.png)
+
+6. **`employees` 테이블을 삭제하세요.**
+   ![alt text](image-6.png)
